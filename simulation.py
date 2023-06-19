4bacb8a0cf2b1b4e05d72eb23d2e4bda11ac3ba0
EricHW55 <111417848+EricHW55@users.noreply.github.com>
2023-06-19 19:18:23 +0900

    simulation.py

--- simulation.py
+++ simulation.py
@@ -3,29 +3,46 @@ import random
 from copy import deepcopy
 from tqdm import trange
 from typing import Tuple
-
+import os
 
 class Simulation:
     def __init__(self):
+        from Texasholdem import env
         self.env = env()
 
 
-    def __call__(self, num:int, community_card:list, my_hand:list, opponent_hand:list, opponent_action_num:int, pot_size:int=0, loss:int=0) -> int: # 시뮬레이션 결과 반환
-        action_results = [0,0,-1*num] # -1*num : 참가비 1$
+    def __call__(self, num:int, community_card:list, my_hand:list, opponent_hand:list, opponent_action_num:int, pot_size:int=0, loss:int=0, shape_to_num:bool=False) -> int: # 시뮬레이션 결과 반환
+        if loss == 0: loss = -1 # ... 
+        action_results = [0,0,loss*num] # -1*num : 참가비 1$
         for n,action in enumerate([0,1]): # action
             for i in range(num):
                 self.community_card = deepcopy(community_card)
-                self.my_hand = deepcopy(my_hand); self.opponent_hand = deepcopy(opponent_hand)
-                _loss, pot_size = self.simulation(opponent_action=opponent_action_num, agent_action=action, pot_size=pot_size, loss=loss) # <<<
+                self.my_hand = deepcopy(my_hand)
+                # self.opponent_hand = deepcopy(opponent_hand)
+                
+                """ 랜덤 """
+                if not shape_to_num:
+                    shapes = ['♠', '♡', '♢', '♣']
+                    left_deck = [(s,i) for s in shapes for i in range(2, 15)]
+                else:
+                    left_deck = [(s,i) for s in range(0, 4) for i in range(2, 15)]  # (Shape, Num) # S : 0, H : 1, D : 2, C : 3
+                for i in self.community_card + self.my_hand: # + self.opponent_hand 부분
+                    left_deck.remove(i) # 남은 카드들
+                self.opponent_hand = random.sample(left_deck,2)
+
+                _loss, pot_size = self.simulation(opponent_action=opponent_action_num, agent_action=action, pot_size=pot_size, loss=loss, shape_to_num=shape_to_num) # <<<
                 action_results[n] += _loss
         # print(action_results)
         return action_results.index(max(action_results))
 
 
-    def simulation(self, opponent_action:int, agent_action:int, pot_size:int, loss:int) -> int: # 시뮬레이션
+    def simulation(self, opponent_action:int, agent_action:int, pot_size:int, loss:int, shape_to_num:bool=False) -> int: # 시뮬레이션
         # 상대방 action에서 fold 제외, 남은 공동카드 2장 랜덤으로 action이 목표, 손실률만 계산
-        shapes = ['♠', '♡', '♢', '♣']
-        left_deck = [(s,i) for s in shapes for i in range(2, 15)]
+        if not shape_to_num:
+            shapes = ['♠', '♡', '♢', '♣']
+            left_deck = [(s,i) for s in shapes for i in range(2, 15)]
+        else:
+            left_deck = [(s,i) for s in range(0, 4) for i in range(2, 15)]  # (Shape, Num) # S : 0, H : 1, D : 2, C : 3
         for i in self.community_card + self.my_hand + self.opponent_hand: # + self.opponent_hand 부분
             left_deck.remove(i) # 남은 카드들
 
@@ -85,7 +102,7 @@ class Simulation:
             loss, pot_size = betting_phase_simulation(action_num=opponent_action, loss=loss, pot_size=pot_size, _random=_random, agent_action=agent_action)
         
 
-        results = env.compare_hand(community_card=self.community_card, opponent_hand=self.opponent_hand, my_hand=self.my_hand)
+        results = self.env.compare_hand(community_card=self.community_card, opponent_hand=self.opponent_hand, my_hand=self.my_hand)
         
         if results == True : # 승
             loss += pot_size
@@ -134,39 +151,50 @@ class Simulation:
 
 
 if __name__ == '__main__':
+    def save_data(_dir:str, txt:str):
+        with open(_dir, 'r', encoding='UTF-8') as f:
+            data = f.readlines()
+        data.append(txt+'\n')
+        with open(_dir, 'w', encoding='UTF-8') as f:
+            for i in data:
+                f.write(i)
+
     env = env()
     sim = Simulation()
-    simulation_num = 600
+    simulation_num = 3000 #600
 
     start_money = 30
     my_money = start_money; opponent_money = start_money
     pot_size = 0#; betting = 1
     
     while True:
-        print('\tHolding Funds \nMe : {}$\tComputer : {}$\n{}'.format(my_money, opponent_money, '-'*35))
+        env() # 덱 초기화
+        print('\n\tHolding Funds \nMe : {}$\tComputer : {}$\n{}'.format(my_money, opponent_money, '-'*35))
         my_money -= 1; opponent_money -= 1; pot_size += 2 # 참가비
         print('\tHolding Funds \nMe : {}$\tComputer : {}$\n\tPot : {}$\n'.format(my_money, opponent_money, pot_size))
 
         community_card = env.draw(input_deck=[], num=3)
         print('Community Card :', *community_card)
         
-        hand = env.draw(input_deck=[], num=2)
+        hand = env.draw(input_deck=[], num=2) # 컴퓨터
         # print('Opponent Hand :', *hand)
 
-        hand2 = env.draw(input_deck=[], num=2)
+        hand2 = env.draw(input_deck=[], num=2) # 사람
         print('My Hand :', *hand2)
 
         action_list = ['Check', 'Raise', 'Fold']
 
         def betting_phase(my_money:int, opponent_money:int, pot_size:int) -> any:
             betting = 1
-            my_action = int(input('Action(0:check, 1:raise, 2:fold) : '))
+            my_action = int(input('\nAction(0:check, 1:raise, 2:fold) : '))
+            save_data('log.txt', txt=f'{community_card} / {hand2} / {hand} / {my_action} / {betting} / {pot_size}') # <<<
+
             if my_action == 2: # fold
                 opponent_money += pot_size; pot_size = 0
                 return my_money, opponent_money, pot_size, True
             elif my_action == 1: # raise
                 betting *= 2; my_money -= betting; pot_size += betting
-                print('\tHolding Funds \nMe : {}$\tComputer : {}$\n\tPot : {}$\n'.format(my_money, opponent_money, pot_size))
+                print('\n\tHolding Funds \nMe : {}$\tComputer : {}$\n\tPot : {}$\n'.format(my_money, opponent_money, pot_size))
 
             action_num = sim(num=simulation_num, community_card=community_card, my_hand=hand, opponent_hand=hand2, opponent_action_num=my_action, pot_size=0, loss=0)
             print("Computer Action : {}".format(action_list[action_num]))
@@ -175,18 +203,20 @@ if __name__ == '__main__':
                 return my_money, opponent_money, pot_size, True
             elif action_num == 1: # raise
                 betting *= 2; opponent_money -= betting; pot_size += betting
-                print('\tHolding Funds \nMe : {}$\tComputer : {}$\n\tPot : {}$\n'.format(my_money, opponent_money, pot_size))
-                my_action = int(input('Action(0:call, 1:fold) : '))
+                print('\n\tHolding Funds \nMe : {}$\tComputer : {}$\n\tPot : {}$\n'.format(my_money, opponent_money, pot_size))
+                my_action = int(input('\nAction(0:call, 2:fold) : '))
+                save_data('log.txt', txt=f'{community_card} / {hand2} / {hand} / {my_action} / {betting} / {pot_size}') # <<<
+
                 if my_action == 0: # call
                     my_money -= betting/2; pot_size += betting/2
-                    print('\tHolding Funds \nMe : {}$\tComputer : {}$\n\tPot : {}$\n'.format(my_money, opponent_money, pot_size))
-                elif my_action == 1: # fold
+                    print('\n\tHolding Funds \nMe : {}$\tComputer : {}$\n\tPot : {}$\n'.format(my_money, opponent_money, pot_size))
+                elif my_action == 2: # fold
                     opponent_money += pot_size; pot_size = 0
                     return my_money, opponent_money, pot_size, True
             elif action_num == 0: # check or call
                 if not betting == 1:
                     opponent_money -= betting/2; pot_size += betting/2
-                print('\tHolding Funds \nMe : {}$\tComputer : {}$\n\tPot : {}$\n'.format(my_money, opponent_money, pot_size))
+                print('\n\tHolding Funds \nMe : {}$\tComputer : {}$\n\tPot : {}$\n'.format(my_money, opponent_money, pot_size))
 
             return my_money, opponent_money, pot_size, False
         my_money, opponent_money, pot_size, _ = betting_phase(my_money, opponent_money, pot_size)
@@ -253,7 +283,7 @@ if __name__ == '__main__':
         if my_money <= 0 or opponent_money <= 0:
             break
     print('\tHolding Funds \nMe : {}$\tComputer : {}$\n'.format(my_money, opponent_money))
-    env() # 덱 초기화
+    
 
 """
 
